fa54515eb49937326272cdd5a4154a3eefb76cc4
omega3love <bigstaron9@gmail.com>
2016-02-28 15:23:47 +0900

    on-bridgeClient

--- bridgeClient.py
+++ bridgeClient.py
@@ -3,33 +3,47 @@
 import socket
 import threading
 from time import sleep
+import sys
 
 class bridgeConnection():
     
     def __init__(self):
 		
-	self.HOST = raw_input("HOST IP : ")
+	#self.HOST = raw_input("HOST IP : ")
+	self.HOST = "143.248.2.116"
 	self.PORT = 50000
 	self.DATA_SIZE = 128 # maximum data length which can be sent in once
 	
+	self.endThread = False
 	self.soc = self.makeConnection()
 	self.dataList = []
 	# Threading allows to get data whenever it's delievered
-	T = threading.Thread(target = self.receiveData)
-	T.start()
+	#T = threading.Thread(target = self.receiveData)
+	#T.start() 
 	
 	""" for test """
-	while 1:
-	    msg = raw_input("send data : ")
-	    if msg == "quit" or "exit":
-		break
-	    self.sendData(msg)
-
+	if not self.soc:
+	    print "Server not opened"
+	else:
+	    T = threading.Thread(target = self.receiveData)
+	    T.start() 
+	    while 1:
+		msg = raw_input("send data : ")
+		self.sendData(msg)
+		if msg == "quit" or msg == "exit":
+		    self.endThread = True
+		    break
+	
+	print "joining the thread..."
+	T.join()
+	print "thread is joined"
+	sys.exit()
+	
     def makeConnection(self):
 	
 	soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	soc.settimeout(10.0) # maximum wating time (seconds)
+	soc.settimeout(6.0) # maximum wating time (seconds)
 	    
 	connected = False
 	while not connected:
@@ -38,6 +52,7 @@ class bridgeConnection():
 		soc.connect( (self.HOST, self.PORT) )
 		connected = True
 		print 'Connected!'
+		#soc.settimeout(None)
 		break
 	    
 	    except socket.timeout:
@@ -56,17 +71,24 @@ class bridgeConnection():
 	""" Send data (string type) to the server """
 	if len(data) <= self.DATA_SIZE:
 	    self.soc.send(data.encode('UTF-8'))
+	    print "Data '%s' is sent successfully" %data
 	else:
 	    print "Data packet size exceeded!"
 	
     def receiveData(self):
 	""" Receive data (string type) from the server """
-	while True:
+	while not self.endThread:
+	    sleep(0.5)
 	    try:
 		data = self.soc.recv(self.DATA_SIZE) # receive data whose length <= DATA_SIZE
+		print "data is : %s" %data
+	    except socket.timeout:
+		print "socket timed out"
+		continue
 	    except:
 		print "Connection is lost"
 		break
+	    
 	    if data:
 		self.dataList.append( data ) # save the received data
 	self.soc.close() # disconnect the connection
