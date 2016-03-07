7e5c2322e1b571bfd3ffd7a1e30ca5a8deba9892
omega3love <bigstaron9@gmail.com>
2016-03-07 20:31:38 +0900

    server update

--- bridgeServer.py
+++ bridgeServer.py
@@ -7,16 +7,16 @@ from time import sleep
 import pygame
 
 myhost = '127.0.0.1'
+connList = []
 
 class Server(Protocol):
 
     def connectionMade(self):
 	
-        # self.transport.write("""connected""")
         self.factory.clients.append(self)
+        
         self.peer = self.transport.getPeer()
         print vars(self.peer) # show who made the connection
-        #self.message_all("info:connMade:%s"%self.peer.host)
 
         if self.peer.host == myhost:
             self.factory.host = self
@@ -31,25 +31,58 @@ class Server(Protocol):
     def connectionLost(self, reason):
 	""" When a client lose a connection """
         print "connection lost ", self
-        self.factory.clients.remove(self)
-        self.message_all("info:connLost:%s"%self.transport.getPeer().host)
+        
+        # Erase lost client from connList
+        # and send the updated connList
+        for conn in connList[:]:
+	    if conn.split(";")[1] == self.transport.getPeer().host:
+		connList.remove(conn)
+		self.message_all("info:connList:%s" %str(connList))
+		break
+	
+	# Erase lost client from self.factory.clients
+	self.factory.clients.remove(self)
 
     def dataReceived(self, data):
 	""" If data is delivered from a client to the server,
 	    deliver the data to other clients """
         sender = self.transport.getPeer().host # address of data sender
-	print data
-        self.message_all(data)
-
+	print "received data is : " + data
+        
+	if "info:askPlay" in data:
+	    spl = data.split(":")[-1].split(";")
+	    PlayerAsking = spl[0]
+	    PlayerAsked = spl[1]
+	    self.message_to(spl[1],data)
+	else:
+	    self.message_all(data)
+	    
+	    # save client to connList
+	    # and send the updated connList
+	    if "info:connMade" in data:
+		splited = data.split(":")[-1].split(";") # [ userName, IP ]
+		for client in self.factory.clients:
+		    newConn = "%s;%s" %(splited[0],splited[1])
+		    if newConn not in connList[:]:
+			connList.append(newConn)
+			self.message_all("info:connList:%s" %str(connList))
+			break
+	    print connList
+	
     def message_all(self, msg):
 	""" Send message to all clients from server """        
         for clients in self.factory.clients:
-            clients.transport.write(msg)
+            clients.transport.write(msg+"^")
 
-    def message_to(self, clients_num, msg):
+    def message_to(self, receiver, msg):
 	""" Send message to a certain client from server """
-        if clients_num in range(1, len(self.factory.clients) + 1):
-            self.factory.clients[clients_num].transport.write(msg + '\n')
+        for client in connList:
+	    spl = client.split(";")
+	    if spl[0] == receiver:
+		for fclient in self.factory.clients:
+		    if fclient.transport.getPeer().host == spl[1]:
+			fclient.transport.write(msg+"^")
+			return
 
 def hostIPaddress():      
     try:
